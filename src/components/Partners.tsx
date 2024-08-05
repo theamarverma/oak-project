81e1e17cd52445bbfd41ed38f6640ee9accc5a9c
theamarverma <amarverma.lnct@gmail.com>
2024-08-05 17:32:57 +0530

    update partners

---
+++ src/components/Partners.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const Partners = () => {
+	return (
+		<>
+			<div className="grid grid-cols-3 md:grid-cols-6 p-16 gap-16 items-center grayscale">
+				<img
+					className=" "
+					src="https://cdn.prod.website-files.com/64ff275e33673d684f459532/6509c7a8653884bf52af4f94_IMG_062D94387ADD-1-p-500.jpeg"
+					alt="synerLeap"
+				/>
+				<img
+					className=""
+					src="https://cdn.prod.website-files.com/64ff275e33673d684f459532/6502ff1418aab6dd711009c5_greentech%20partner%20logo.webp"
+					alt="greentech"
+				/>
+				<img
+					className="h-36 ml-10"
+					src="https://cdn.prod.website-files.com/64ff275e33673d684f459532/6502ff145aa9cdd684a0690c_ORB%20Member%20Partner%20Logo.webp"
+					alt="orb"
+				/>
+				<img
+					src="https://cdn.prod.website-files.com/64ff275e33673d684f459532/6502ff14a3d8f08049fd9eb3_Responsible%20Alpha%20partner%20logo.webp"
+					alt="Responsible Alpha"
+				/>
+				<img
+					src="https://cdn.prod.website-files.com/64ff275e33673d684f459532/6502ff143523a3b6cf9e81dc_green%20net%20zero%20partner%20logo.webp"
+					alt="net-zero"
+				/>
+				<img
+					src="https://cdn.prod.website-files.com/64ff275e33673d684f459532/6502ff1472a4d48f227de537_west%20london%20partner%20logo-p-500.webp"
+					alt="westLondon"
+				/>
+			</div>
+		</>
+	);
+};
+
+export default Partners;
